ccc91f23e29d209e7029b5e26abed901e40a1bae
brian bacungan <brian@prismtechinc.io>
2021-11-07 00:49:12 -0700

    Add user analytics chart container

---
+++ src/components/chart/chartSC.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import { BOX_SHADOW } from "../../shared/sharedStyles";
+
+const ChartSC = styled.div`
+  margin: 10px;
+  padding: 20px;
+  box-shadow: ${BOX_SHADOW};
+  -webkit-box-shadow: ${BOX_SHADOW};
+  border-radius: 10px;
+
+.chat-title {
+  margin-bottom: 20px;
+}
+`;
+
+export { ChartSC };

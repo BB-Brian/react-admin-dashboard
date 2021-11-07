c2d03c31bd1cbfe4577e82566318974d0c58a99f
brian bacungan <brian@prismtechinc.io>
2021-11-07 01:52:40 -0700

    Add dummy data component.

---
+++ src/dummyData.js
@@ -0,0 +1,51 @@
+export const userData = [
+    {
+      name: 'JAN',
+      "Active User": 4000,
+    },
+    {
+      name: 'FEB',
+      "Active User": 3000,
+    },
+    {
+      name: 'MAR',
+      "Active User": 2000,
+    },
+    {
+      name: 'APR',
+      "Active User": 2780,
+
+    },
+    {
+      name: 'MAY',
+      "Active User": 1890,
+    },
+    {
+      name: 'JUN',
+      "Active User": 2390,
+    },
+    {
+      name: 'JUL',
+      "Active User": 3490,
+    },
+    {
+      name: 'AUG',
+      "Active User": 5001,
+    },
+    {
+      name: 'SEP',
+      "Active User": 4680,
+    },
+    {
+      name: 'OCT',
+      "Active User": 5790,
+    },
+    {
+      name: 'NOV',
+      "Active User": 4521,
+    },
+    {
+      name: 'DEC',
+      "Active User": 5890,
+    }
+  ];

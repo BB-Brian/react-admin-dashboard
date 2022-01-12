394964be90a8b6e44372ad7a7c63efb898d51063
brian bacungan <brian@prismtechinc.io>
2022-01-11 23:55:12 -0800

    Add user list route and app style component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TopBar from "./components/topbar/TopBar";
 import Sidebar from "./components/sidebar/Sidebar";
 import Home from "./pages/home/Home";
-import "./app.css";
+import UserList from "./pages/userList/UserList";
+import { AppSC } from "./AppSC.js";
 
 function App() {
   return (
-    <div className="App">
-      <TopBar />
-      <div className="main-container">
-        <Sidebar />
-        <Home />
-      </div>
-    </div>
+    <Router>
+      <AppSC>
+        <TopBar />
+        <div className="main-container">
+          <Sidebar />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/users" element={<UserList />} />
+          </Routes>
+        </div>
+      </AppSC>
+    </Router>
   );
 }
 
-export default App; 
+export default App;

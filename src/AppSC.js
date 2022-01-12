f1995cd681a4b0b317a044a92e32df4efd3ce0a5
brian bacungan <brian@prismtechinc.io>
2022-01-11 23:53:13 -0800

    Add user list route and app style component

---
+++ src/AppSC.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const AppSC = styled.div`
+  background: rgb(12,8,28);
+  background: linear-gradient(138deg, rgba(0,0,0,1) 15%, rgba(29,22,59,1) 97%);  
+  /* background: linear-gradient(138deg, rgba(12,8,28,1) 54%, rgba(29,22,59,1) 95%);   */
+  /* background-image: url("https://images.pexels.com/photos/6208160/pexels-photo-6208160.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");   */
+
+.main-container {
+  display: flex;
+}
+
+`
+export { AppSC };

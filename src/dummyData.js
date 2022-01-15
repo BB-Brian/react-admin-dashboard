7034fe5762c32c7a7db76965dc6e9c66cf2cd8f0
brian bacungan <brian@prismtechinc.io>
2022-01-14 23:04:48 -0800

    Update dummy data model.

--- src/dummyData.js
+++ src/dummyData.js
@@ -49,3 +49,78 @@ export const userData = [
       "Active User": 5890,
     }
   ];
+
+export const newMembersData = [
+  {
+    id: 1,
+    name: "Anna Keller",
+    username: "akeller",
+    email: "anna@email.com",
+    jobTitle: "Software Enginner",
+    image: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+    date: "Nov 1, 2021",
+    amount: 754.99,
+    transaction: 60,
+    status: "active"
+  },
+  {
+    id: 2,
+    name: "Rebecca Swift",
+    username: "rswift",
+    email: "rebecca@email.com",
+    jobTitle: "UX/UI Designer",
+    image: "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    date: "Sept 20, 2021",
+    amount: 460.00,
+    transaction: 180,
+    status: "disabled"
+  },
+  {
+    id: 3,
+    name: "Jisoo Rose",
+    username: "jrose",
+    email: "jisoo@email.com",
+    jobTitle: "QA Enginner",
+    image: "https://images.pexels.com/photos/3616937/pexels-photo-3616937.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    date: "Sept 19, 2021",
+    amount: 523.49,
+    transaction: 6000,
+    status: "active"
+  },
+  {
+    id: 4,
+    name: "Q Kilauea",
+    username: "qkilauea",
+    email: "q@email.com",
+    jobTitle: "devOps Engineer",
+    image: "https://images.pexels.com/photos/9424119/pexels-photo-9424119.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    date: "Nov 6, 2021",
+    amount: 32.00,
+    transaction: 120,
+    status: "inactive"
+  },
+  {
+    id: 5,
+    name: "Jupiter Baku",
+    username: "jbaku",
+    email: "jupiter@email.com",
+    jobTitle: "Game Dev",
+    image: "https://images.pexels.com/photos/356147/pexels-photo-356147.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    date: "Nov 4, 2021",
+    amount: 1999.99,
+    transaction: 60,
+    status: "active"
+  },
+  {
+    id: 6,
+    name: "Raiden Smith",
+    username: "rsmith",
+    email: "raiden@email.com",
+    jobTitle: "Photographer",
+    image: "https://images.pexels.com/photos/598917/pexels-photo-598917.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    date: "Oct 31, 2021",
+    amount: 10000.99,
+    transaction: 60,
+    status: "inactive"
+  },
+];
